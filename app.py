46af3c6f8cf393e2cc54d0e23250554075ba3f0a
Kelti Wise <123501504+keltiwise@users.noreply.github.com>
2025-02-26 12:04:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -416,13 +416,7 @@ with col[1]:
     
     # Check if the logo file exists; if it does, display the image
     if os.path.exists(logo_path):
-        # Center the logo using custom HTML
-        st.markdown(
-            f"""
-            <div style="display: flex; justify-content: center;">
-                <img src="{logo_path}" width="400">
-            </div>
-            """, unsafe_allow_html=True)
+        st.image(logo_path, width=400)  # Adjust the image width as necessary
     else:
         # If the logo is not found, display a warning message
         st.warning(f"Logo for {team_display_name} not found.")
